d008de733e280317920d57029271bdb0e00efb49
DataM0del <183248792+DataM0del@users.noreply.github.com>
2024-11-02 11:36:45 -0500

    feat(opcode_handlers): implement `DUPTABLE`

--- koralys.py
+++ koralys.py
@@ -515,6 +515,7 @@ def read_proto(
             "LENGTH": lambda _: f"R{A} = #R{B}",
             # https://github.com/luau-lang/luau/blob/a251bc68a2b70212e53941fd541d16ce523a1e01/Compiler/src/BytecodeBuilder.cpp#L2134-L2136
             "NEWTABLE": lambda _: f"R{A} = table with {(B == 0 and 0 or 1 << max(0, B - 1)) + 1} entries",
+            "DUPTABLE": lambda _: f"R{A} = R{B} -- duplicate",
             "CONCAT": lambda _: f"R{A} = R{B} .. R{C}",
             "NOT": lambda _: f"R{A} = not R{B}",
             "FORGPREP": lambda _: f"R{A} = R{A+1}; R{A+1} = R{A+2}; R{A+2} = R{A+3}; R{A+3} = nil; goto [{(codeIndex + 1 + Bx) & 0xFF}]",
